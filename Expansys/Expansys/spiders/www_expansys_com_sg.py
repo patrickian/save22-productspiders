cdaa88c12b353b7ee809fa2ccdbcc69883ef8a02
patrickian <patrick.concepcion33@gmail.com>
2015-12-03 01:37:03 -0500

    added Rules in www_expansys_com_sg.

--- Expansys/Expansys/spiders/www_expansys_com_sg.py
+++ Expansys/Expansys/spiders/www_expansys_com_sg.py
@@ -2,8 +2,17 @@ import scrapy
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+from ..items import ExpansysItem
 
 class WwwExpansysComSg(CrawlSpider):
-  name = 'www_expansys_com_sg_crawler'
-  allowed_domains = ['expansys.com.sg']
-  start_urls = ['http://www.expansys.com.sg/']
+    name = 'www_expansys_com_sg_crawler'
+    allowed_domains = ['expansys.com.sg']
+    start_urls = ['http://www.expansys.com.sg/']
+    rules =(Rule(LinkExtractor(allow = (r'/\S+-\d+/', 
+        )) , #restrict_xpaths=('//div[@id = "nav"]/ul/li')),
+        callback = 'parse_categories', follow = True),
+      )
+    #, deny =(''))
+    def parse_categories(self,response):
+        pass
