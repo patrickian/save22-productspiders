ff14fe5ea59120c01b0b4a8180e3d6a13f89aa76
patrickian <patrick.concepcion33@gmail.com>
2015-12-07 03:39:00 -0500

    added rules and getting items. (Experiment)

--- Expansys/Expansys/spiders/www_expansys_com_sg.py
+++ Expansys/Expansys/spiders/www_expansys_com_sg.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from datetime import datetime
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
@@ -9,45 +9,45 @@ class WwwExpansysComSg(CrawlSpider):
     name = 'www_expansys_com_sg_crawler'
     allowed_domains = ['expansys.com.sg']
     start_urls = ['http://www.expansys.com.sg/']
-    rules =(Rule(
-      LinkExtractor(
-        allow = (
-          r'.+/\S+-\d+/', 
-        ),
-        deny = (
-          r'.+/?filter',
-        )
-      ),
-      callback = 'parse_categories',
-      follow = True),
+    rules =(
+      Rule(LinkExtractor(allow = (r'.+', ),deny = ( r'.+/?filter\S+',r'.+aspx.\S+',r'.+/forum.\S+',r'.+/blog\S+',r'.+/?c=\S+',r'.+/more/\S+')),callback = 'parse_products',follow = True),
+      #Rule(LinkExtractor( allow = (r'.+/?page=[1-9]+#\w+',),deny = (r'.+/?filter',)),follow = True),
+      #Rule(LinkExtractor( allow = (r'.+/\S+-\d+.',), deny = ( r'.+/?filter\S+',)),callback = 'parse_products', follow = False),
       )
-      
+    
     def parse_categories(self,response):
-        #l = ItemLoader(item=ExpansysItem(), response=response)
-        _title = response.xpath('//div[@id="product"]//h1[@itemprop="name"]/text()').extract()
-        _url = response.url
-        _price = response.xpath('//p[@id ="price"]//span/text()').extract()
-        _price = _price + response.xpath('//p[@id ="price"]//span/sup/text()').extract()
-        _model = response.xpath('//ul[@class ="product-sku"]/li[4]/span/text()').extract()
-        _sku = response.xpath('//ul[@class ="product-sku"]/li[1]/span/text()').extract()
-        _mpn = response.xpath('//ul[@class ="product-sku"]/li[3]/span/text()').extract()
-        #_upc =
-        _ean = response.xpath('//ul[@class ="product-sku"]/li[2]/span/text()').extract()
-        _currency = response.xpath('//p[@id ="price"]/meta/@content').extract()
-        #_crawl_time =
-        #_image_urls = 
-        #_categories =
-        _instock = response.xpath('//li[@id ="stock"]/text()').extract()
+        pass
+    
+    def parse_products(self,response):
+        print 'PUMASOK ------------------------------------------------------------'
+        _upc = []
+        _sku = []
+        _ean = []
+        _mpn = []
+        _title = response.xpath('//div[@id="product"]/div/h1[@itemprop="name"]/text()').extract()
+         
+        print _title
+        print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!asdasdasd' 
+        #_prod = response.xpath('//ul[@class ="product-sku"]/li/span/@content').extract() 
+        #for x in _prod: 
+        #  if x[0:3] == 'sku': _sku.append(x[4:])
+        #  elif x[0:3] == 'ean': _ean.append(x[4:])
+        #  elif x[0:3] == 'upc': _upc.append(x[4:])
+        #  elif x[0:3] == 'mpn': _mpn.append(x[4:]) 
+          
         if _title:
-            print '----------------------------------------------------'
-            print _title
-            print _url
-            print _price
-            print _currency
-            print _instock
-            print _sku
-        #l.add_xpath('_name', '//div[@id="product"]//h1[@itemprop="name"]/text()')
-        #l.add_xpath('price', '//p[@id="price"]')
-        #l.add_css('stock', 'p#stock]')
-        #l.add_value('last_updated', 'today') # you can also use literal values
-        #return l.load_item()
+          item = ExpansysItem()
+          _price = response.xpath('//p[@id ="price"]/strong/span/text()').extract()[0]
+          item['_title'] = response.xpath('//div[@id="product"]//h1[@itemprop="name"]/text()').extract()
+          item['_url'] = response.url 
+          item['_price'] = _price + response.xpath('//p[@id ="price"]/strong/span/sup/text()').extract()[0]
+          item['_model'] = response.xpath('//ul[@class ="product-sku"]//a[@itemprop ="brand"]/text()').extract()          
+          item['_currency'] = response.xpath('//p[@id ="price"]/meta/@content').extract()
+          item['_crawl_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+          item['_image_urls'] = response.xpath('//div[@id ="image"]//img/@src').extract()
+          _cat = response.xpath('//ul[@id ="breadcrumbs"]/li[@class ="level_0"]//span/text()').extract()
+          _cat.extend(response.xpath('//ul[@id ="breadcrumbs"]/li[@class ="level_1"]//span/text()').extract())
+          item['_categories'] = '\\'.join(_cat)
+          item['_instock'] = response.xpath('//li[@id ="stock"]/text()').extract()
+          yield item
+        
